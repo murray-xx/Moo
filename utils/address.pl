c34415c8329abc6056862728d28e451e19215be3
Murray Barton <muzza@oak.insubstantial.com.au>
2010-01-25 11:30:16 +0800

    updates and fixes to mre, rcg and address.pl

--- utils/address.pl
+++ utils/address.pl
@@ -1,10 +1,11 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 #
 # address.pl
 #
 
 $VERSION = '0.8.1';
 
+use warnings;
 use strict;
 use Getopt::Long qw(:config no_ignore_case);
 use Pod::Usage;
@@ -12,9 +13,9 @@ use File::Basename;
 use Data::Dumper;
 use Text::xSV;
 
-$|=1;
+$| = 1;
 
-my $settings = { 
+my $settings = {
     debug             => 0,
     search_all_fields => 0,
     print_all_fields  => 0,
@@ -25,13 +26,13 @@ GetOptions(
     'p'         => \$settings->{print_all_fields},
     'd'         => \$settings->{debug},
     'f=s'       => \$settings->{file},
-    'usage'     => sub { pod2usage(-verbose => 0) && exit 0 }, 
-    'help|?'    => sub { pod2usage(-verbose => 1) && exit 0 }, 
-    'man'       => sub { pod2usage(-verbose => 2) && exit 0 }, 
-    'version|V' => sub { print basename($0), " $main::VERSION\n";  exit 0}, 
-) or pod2usage(-verbose => 0) && exit 0;
-    
-if ( ! defined $settings->{file} ){
+    'usage'     => sub { pod2usage( -verbose => 0 ) && exit 0 },
+    'help|?'    => sub { pod2usage( -verbose => 1 ) && exit 0 },
+    'man'       => sub { pod2usage( -verbose => 2 ) && exit 0 },
+    'version|V' => sub { print basename($0), " $main::VERSION\n"; exit 0 },
+) or pod2usage( -verbose => 0 ) && exit 0;
+
+if ( !defined $settings->{file} ) {
     my $dir = dirname($0);
     $settings->{file} = "$dir/contacts.csv";
 }
@@ -42,53 +43,61 @@ $pattern = '\b' . $pattern;
 
 print "[$pattern]\n" if $settings->{debug};
 
-%{$settings->{print_fields}} = map {$_,1} "E-mail Address", "Mobile Phone", "Business Phone", "Home Phone", "E-mail 2 Address", "E-mail 3 Address";
-%{$settings->{never_print}} = map {$_,1} 'Priority';
-@{$settings->{name_fields}} = ('First Name', 'Middle Name', 'Last Name', 'Company');
-print Data::Dumper->Dump([\$settings], [qw(*settings)]) if $settings->{debug};
+%{ $settings->{print_fields} } = map { $_, 1 } "E-mail Address", "Mobile Phone",
+  "Business Phone", "Home Phone", "E-mail 2 Address", "E-mail 3 Address";
+%{ $settings->{never_print} } = map { $_, 1 } 'Priority';
+@{ $settings->{name_fields} } =
+  ( 'First Name', 'Middle Name', 'Last Name', 'Company' );
+print Data::Dumper->Dump( [ \$settings ], [qw(*settings)] )
+  if $settings->{debug};
 
 my $csv = new Text::xSV;
-$csv->open_file($settings->{file});
+$csv->open_file( $settings->{file} );
 $csv->read_header();
 
 $csv->set_row_size(89);
 
-while ($csv->get_row()) {
+ROW:
+while ( $csv->get_row() ) {
     my %record = $csv->extract_hash();
+
     #print Data::Dumper->Dump([\%record], [qw(*record)]);
 
-    if ( $settings->{search_all_fields} ){
-        FIELD:
-        for my $field ( keys %record ){
-		    if ( defined $record{$field} and $record{$field} =~ /$pattern/i ){
-			    print_record(%record);
-                # it matched, we printed it, no need to look any futher!
-			    last FIELD;
-			}
-        }
-	}
-	else {
-        for my $field ( @{ $settings->{name_fields} } ){
-            if ( defined $record{$field} and $record{$field} =~ /$pattern/i ){
+    if ( $settings->{search_all_fields} ) {
+      FIELD:
+        for my $field ( keys %record ) {
+            if ( defined $record{$field} and $record{$field} =~ /$pattern/i ) {
                 print_record(%record);
 
+                # it matched, we printed it, no need to look any futher!
+                last FIELD;
             }
         }
-	}
-    
+    }
+    else {
+        for my $field ( @{ $settings->{name_fields} } ) {
+            if ( defined $record{$field} and $record{$field} =~ /$pattern/i ) {
+                print_record(%record);
+                # jump to the next row otherwise if multiple fields
+                # match we will pring the record multiple times == suckage!
+                next ROW;
+            }
+        }
+    }
+
 }
 
 exit;
 
 sub print_record {
-	my %record = @_;
+    my %record    = @_;
     my $full_name = "";
     my %name_fields;
 
-    for ( @{ $settings->{name_fields} } ){
+    for ( @{ $settings->{name_fields} } ) {
         ++$name_fields{$_};
-        if ( defined $record{$_} ){
-            if ( ! length $full_name ){
+        if ( defined $record{$_} ) {
+            if ( !length $full_name ) {
                 $full_name = $record{$_};
             }
             else {
@@ -96,30 +105,31 @@ sub print_record {
             }
         }
     }
-    
+
     print "$full_name:\n";
-	
-    for my $field ( sort { special_sort() } keys %record ){
+
+    for my $field ( sort { special_sort() } keys %record ) {
         next if ( defined $name_fields{$field} );
-        if ( ! $settings->{print_all_fields} ){
+        if ( !$settings->{print_all_fields} ) {
             next unless defined $settings->{print_fields}->{$field};
         }
         next if defined $settings->{never_print}->{$field};
-        
-	    my $value = $record{$field};
-        if ( $value ){
+
+        my $value = $record{$field};
+        if ($value) {
             $value =~ s/\n/\n                            : /g;
-        	printf "\t%-20s: %s\n", $field, $value;
+            printf "\t%-20s: %s\n", $field, $value;
         }
-	}
+    }
 }
 
 sub glob2pat {
-# gleefully stolen from Recipe 6.9. Matching Shell Globs as Regular Expressions
-# of the Perl Cookbook :-)
+
+ # gleefully stolen from Recipe 6.9. Matching Shell Globs as Regular Expressions
+ # of the Perl Cookbook :-)
 
     my $globstr = shift;
-    my %patmap = (
+    my %patmap  = (
         '*' => '.*',
         '?' => '.',
         '[' => '[',
@@ -130,9 +140,12 @@ sub glob2pat {
 }
 
 sub special_sort {
+
     # we want phone records first
     # email address next
-    (($a =~ /e-mail/i) <=> ($b =~ /e-mail/i)) || (($a =~ /e-mail \d/i) <=> ($b =~ /e-mail \d/i)) || $a cmp $b;
+    ( ( $a =~ /e-mail/i ) <=> ( $b =~ /e-mail/i ) )
+      || ( ( $a =~ /e-mail \d/i ) <=> ( $b =~ /e-mail \d/i ) )
+      || $a cmp $b;
 }
 
 =head1 NAME
@@ -146,33 +159,33 @@ address.pl [-s -p -a --usage --help -? -man -version -V] [search_pattern]
 =head1 DESCRIPTION
 
 Simple command line search of the CSV file export of my GMail contacts
-  
+
 =head1 OPTIONS
 
--s|-a 
+-s|-a
     search all fields
-    
--p 
+
+-p
     print all fields with values for matching records
-    
+
 -d
     print some debugging information
-    
---help|-? 
+
+--help|-?
     Print this message
-    
+
 --man
     Print the man page
-    
+
 --usage
     print the usage line
-    
+
 --version|-V
     Print the program version number
 
 =head1 BUGS
 
-None that I know of :)  Use at your own risk! 
+None that I know of :)  Use at your own risk!
 
 =head1 To Do/Wishlist
 
