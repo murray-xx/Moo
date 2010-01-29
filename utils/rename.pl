aa58e61ff738da27a1a15905fa1d45ee151be741
Murray Barton <muzza@oak.insubstantial.com.au>
2010-01-29 20:39:36 +0800

    minor changes to rename.pl

--- utils/rename.pl
+++ utils/rename.pl
@@ -1,16 +1,28 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+# 
+# rename.pl
 #
 # originally by Larry Wall in the Perl Cookbook
-# modified randomly by Murray Barton
+# modified by Murray Barton
+#    email: murray.barton at gmail.com
+#    http://incommunique.blogspot.com
+# breakage and stupidities are no doubt my fault and not Larry's :)
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
 #
 
+use strict;
+use warnings;
 use Getopt::Std;
 
 my $useage=<<USEAGE;
 Usage: rename expr [-hp] [files]
     -h print this screen
     -p preview mode, say what would happen but don't do it
-    eg. rename "s/job00001./kord70m./" job00001.*
+    eg. rename "s/job00001./williams./" job00001.*
     lowercase filenames: rename "s/(.*)/lc(\$1)/e" *
 USEAGE
 
@@ -22,17 +34,17 @@ my $preview=0;
   print $useage and exit if $args{h};
 }
 
-$op = shift or die $useage;
+my $op = shift or die $useage;
 
 print "regular expression is: [$op]\n" if $preview;
 
 chomp (@ARGV = <STDIN>) unless @ARGV;
 for (@ARGV) {
-	$was = $_;
+	my $was = $_;
 	eval $op;
 	die $@ if $@;
 	next if $was eq $_;
 	print $was, " to ", $_, "\n";
-  next if $preview;
+    next if $preview;
 	rename($was, $_) or die "Could not rename $was to $_: $!\n";
 }
