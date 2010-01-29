9b3d7cefb56fca2c4fd9fd2085de063466775940
Murray Barton <muzza@oak.insubstantial.com.au>
2010-01-29 20:47:07 +0800

    documentation changes to address.pl

--- utils/findpkginfo.pl
+++ utils/findpkginfo.pl
@@ -1,6 +1,73 @@
 #!/usr/bin/perl
+#
 
-$VERSION = 0.3;
+=head1 NAME
+
+ findpkginfo.pl 
+
+=head1 SYNOPSIS
+
+ findpkginfo.pl -f <filename> <search string>
+ pkginfo -l | findpkginfo.pl - <search string>
+
+=head1 DESCRIPTION
+
+ Solaris's pkginfo -l command is not very search friendly
+ findpkginfo.pl is to fix that.  Prints out the full output
+ for pkginfo -l for any package that matches the search string
+ in package identifier (PKGINST), package name (NAME) 
+ or description (DESC)
+
+ find package info in the output of pkginfo -l from either a pipe or a file
+ <search string> can be any valid perl regular expression
+
+=head1 OPTIONS
+
+ -
+    read pkginfo from STDIN 
+    ie pkginfo -l | findpkginfo.pl -
+ -f <filename>
+    read pkginfo from <filename>
+ --help 
+     Print this message
+ --man
+    Print the man page
+ --version|-V
+    Print the program version number
+
+=head1 BUGS
+
+None that I know of :)  Use at your own risk! 
+
+=head1 Credits
+
+Juerd for "POD in 5 minutes" <http://perlmonks.org/?node_id=252477> and
+ybiC for "The Dynamic Duo" <http://perlmonks.org/?node_id=155288>
+
+=head1 AUTHOR
+
+Murray Barton
+email: murray.barton at gmail.com
+http://incommunique.blogspot.com
+
+=head1 COPYRIGHT
+
+Copyright (c) 2003. Murray Barton. All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=head1 DISCLAIMER
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty
+ of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+=cut
+
+$VERSION = '0.3.1';
 
 use strict;
 use warnings;
@@ -98,61 +165,4 @@ sub print_pkg {
     }
 }
 
-=head1 NAME
-
- findpkginfo.pl 
-
-=head1 SYNOPSIS
-
- findpkginfo.pl -f <filename> <search string>
- pkginfo -l | findpkginfo.pl - <search string>
-
-=head1 DESCRIPTION
-
- Solaris's pkginfo -l command is not very search friendly
- findpkginfo.pl is to fix that.  Prints out the full output
- for pkginfo -l for any package that matches the search string
- in package identifier (PKGINST), package name (NAME) 
- or description (DESC)
-
- find package info in the output of pkginfo -l from either a pipe or a file
- <search string> can be any valid perl regular expression
-
-=head1 OPTIONS
-
- -
-    read pkginfo from STDIN 
-    ie pkginfo -l | findpkginfo.pl -
- -f <filename>
-    read pkginfo from <filename>
- --help 
-     Print this message
- --man
-    Print the man page
- --version|-V
-    Print the program version number
-
-=head1 BUGS
-
-None that I know of :)  Use at your own risk! 
-
-=head1 Credits
-
-Juerd for "POD in 5 minutes" <http://perlmonks.org/?node_id=252477> and
-ybiC for "The Dynamic Duo" <http://perlmonks.org/?node_id=155288>
-
-=head1 AUTHOR
-
-Murray Barton - <http://www.insubstantial.com.au/>
-
-=head1 COPYRIGHT
-
-Copyright (c) 2003. Murray Barton. All rights reserved.
-
-This program is free software; you can redistribute it and/or 
-modify it under the same terms as Perl itself.
-
-See http://www.perl.com/perl/misc/Artistic.html
-
-=cut
 
